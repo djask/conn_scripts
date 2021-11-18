a345467f22eb2b91f77e112c137f31cd204c0e05
djask <2slkdjflskjflskjfdlskjdf@slkjdfkjsf.com>
2021-11-19 10:26:51 +1100

    add csv to mremote

--- mremote_xml.py
+++ mremote_xml.py
@@ -1,6 +1,6 @@
-from securecrt_scraper import scrt_scrape
 import sys
-
+import os
+from csv import reader
 
 
 MREMOTE_HEADER = """<?xml version="1.0" encoding="utf-8"?>
@@ -10,14 +10,27 @@ MREMOTE_HEADER = """<?xml version="1.0" encoding="utf-8"?>
 MREMOTE_CONN_LINE = """<Node Name="{}" VmId="" UseVmId="false" UseEnhancedMode="false" Type="Connection" Descr="" Icon="mRemoteNG" Panel="General"  Username="" Domain="" Password="" Hostname="{}" Protocol="{}" RdpVersion="rdc6" SSHTunnelConnectionName="" SSHOptions="" PuttySession="Default Settings" Port="{}" ConnectToConsole="false" UseCredSsp="true" RenderingEngine="IE" RDPAuthenticationLevel="NoAuth" RDPMinutesToIdleTimeout="0" RDPAlertIdleTimeout="false" LoadBalanceInfo="" Colors="Colors16Bit" Resolution="FitToWindow" AutomaticResize="true" DisplayWallpaper="false" DisplayThemes="false" EnableFontSmoothing="false" EnableDesktopComposition="false" DisableFullWindowDrag="false" DisableMenuAnimations="false" DisableCursorShadow="false" DisableCursorBlinking="false" CacheBitmaps="false" RedirectDiskDrives="false" RedirectPorts="false" RedirectPrinters="false" RedirectClipboard="false" RedirectSmartCards="false" RedirectSound="DoNotPlay" SoundQuality="Dynamic" RedirectAudioCapture="false" RedirectKeys="false" Connected="false" PreExtApp="" PostExtApp="" MacAddress="" UserField="" Favorite="false" ExtApp="" StartProgram="" VNCCompression="CompNone" VNCEncoding="EncHextile" VNCAuthMode="AuthVNC" VNCProxyType="ProxyNone" VNCProxyIP="" VNCProxyPort="0" VNCProxyUsername="" VNCProxyPassword="" VNCColors="ColNormal" VNCSmartSizeMode="SmartSAspect" VNCViewOnly="false" RDGatewayUsageMethod="Never" RDGatewayHostname="" RDGatewayUseConnectionCredentials="Yes" RDGatewayUsername="" RDGatewayPassword="" RDGatewayDomain="" />"""
 MREMOTE_FOOTER = """</mrng:Connections>"""
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print ("Usage: securecrt_scraper.py [base scrt folder]")
-        sys.exit()
+def scrt_to_mremote():
     crt = scrt_scrape(sys.argv[1])
     crt.scrape_sessions_recursive()
-    
-    print (MREMOTE_HEADER)
-    for s in crt.sessions:
-        print (MREMOTE_CONN_LINE.format(s[0], s[2], s[1], s[3]))
-    print (MREMOTE_FOOTER)
+
+
+#from own csv template
+def csv_to_mremote(csv_file, export_file):
+    with open(export_file, "w") as f:
+        f.write(MREMOTE_HEADER + "\n")
+        with open(os.path.join(sys.path[0], csv_file), 'r', newline='') as csvfile:
+            sessions = reader(csvfile, delimiter='\t')
+            for s in sessions:
+                if s[0] != '':
+                    f.write(MREMOTE_CONN_LINE.format(s[0], s[1], s[2], s[3]) + "\n")
+                
+        f.write(MREMOTE_FOOTER + "\n")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print ("Usage: securecrt_scrapyer.py [csv] [output]")
+        sys.exit()
+        
+    csv_to_mremote(sys.argv[1], sys.argv[2])
