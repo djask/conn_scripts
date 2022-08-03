afd09a2fa21574d3a250f514a7eece40b5d02b29
djask <>
2022-08-03 11:06:12 +1000

    Cleanup and change README

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 This repo is a collection of scripts for converting configuration files from SecureCRT to MremotenG. 
 
 Other programs may be added in the future
+
+
+### mremote_xml.py
+Script to take a CSV file and convert into a folder of sessions to be imported into mRemote 
+
+Example format (HOST, IP, PROTOCOL, PORT):
+
+```
+93180yc-ex-1,100.64.43.25,Telnet,2035
+93180yc-ex-2,100.64.43.25,Telnet,2036
+93180yc-fx3-1,100.64.43.25,Telnet,2037
+```
+
+```ps
+PS Z:\> python3 .\mremote_xml.py .\input.csv foldername.xml
+```
+
+### experimental
+Folder of scripts to directly convert secureCRT into mRemote. 
--- mremote_xml.py
+++ mremote_xml.py
@@ -10,17 +10,12 @@ MREMOTE_HEADER = """<?xml version="1.0" encoding="utf-8"?>
 MREMOTE_CONN_LINE = """<Node Name="{}" VmId="" UseVmId="false" UseEnhancedMode="false" Type="Connection" Descr="" Icon="mRemoteNG" Panel="General"  Username="" Domain="" Password="" Hostname="{}" Protocol="{}" RdpVersion="rdc6" SSHTunnelConnectionName="" SSHOptions="" PuttySession="Default Settings" Port="{}" ConnectToConsole="false" UseCredSsp="true" RenderingEngine="IE" RDPAuthenticationLevel="NoAuth" RDPMinutesToIdleTimeout="0" RDPAlertIdleTimeout="false" LoadBalanceInfo="" Colors="Colors16Bit" Resolution="FitToWindow" AutomaticResize="true" DisplayWallpaper="false" DisplayThemes="false" EnableFontSmoothing="false" EnableDesktopComposition="false" DisableFullWindowDrag="false" DisableMenuAnimations="false" DisableCursorShadow="false" DisableCursorBlinking="false" CacheBitmaps="false" RedirectDiskDrives="false" RedirectPorts="false" RedirectPrinters="false" RedirectClipboard="false" RedirectSmartCards="false" RedirectSound="DoNotPlay" SoundQuality="Dynamic" RedirectAudioCapture="false" RedirectKeys="false" Connected="false" PreExtApp="" PostExtApp="" MacAddress="" UserField="" Favorite="false" ExtApp="" StartProgram="" VNCCompression="CompNone" VNCEncoding="EncHextile" VNCAuthMode="AuthVNC" VNCProxyType="ProxyNone" VNCProxyIP="" VNCProxyPort="0" VNCProxyUsername="" VNCProxyPassword="" VNCColors="ColNormal" VNCSmartSizeMode="SmartSAspect" VNCViewOnly="false" RDGatewayUsageMethod="Never" RDGatewayHostname="" RDGatewayUseConnectionCredentials="Yes" RDGatewayUsername="" RDGatewayPassword="" RDGatewayDomain="" />"""
 MREMOTE_FOOTER = """</mrng:Connections>"""
 
-def scrt_to_mremote():
-    crt = scrt_scrape(sys.argv[1])
-    crt.scrape_sessions_recursive()
-
-
 #from own csv template
 def csv_to_mremote(csv_file, export_file):
     with open(export_file, "w") as f:
         f.write(MREMOTE_HEADER + "\n")
         with open(os.path.join(sys.path[0], csv_file), 'r', newline='') as csvfile:
-            sessions = reader(csvfile, delimiter='\t')
+            sessions = reader(csvfile, delimiter=',')
             for s in sessions:
                 if s[0] != '':
                     f.write(MREMOTE_CONN_LINE.format(s[0], s[1], s[2], s[3]) + "\n")
@@ -30,7 +25,7 @@ def csv_to_mremote(csv_file, export_file):
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        print ("Usage: securecrt_scrapyer.py [csv] [output]")
+        print ("Usage: mremote_xml.py [csv] [output]")
         sys.exit()
         
     csv_to_mremote(sys.argv[1], sys.argv[2])
